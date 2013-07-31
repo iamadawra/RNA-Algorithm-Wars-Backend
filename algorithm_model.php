09354306afcbbad043f7bdcc3d66da112cb742cc
Vineet Kosaraju <vineetsk1gmail.com>
2013-07-31 15:45:38 -0700

    Finished initial implenetation of algorithm getter methods

--- algorithm_model.php
+++ algorithm_model.php
@@ -20,27 +20,56 @@ class EternaAlgorithmsModel{
 
 	//Gets algorithms with ratings between ratingA and ratingB
 	function get_algorithms_between($ratingA, $ratingB){
-		//Implementation
+		$query = "SELECT n.title, n.created, algorithm.field_algorithm_rating_value AS rating, algorithm.field_algorithm_code_value AS code, algorithm.field_algorithm_votes_value AS numvotes, algorithm.field_algorithm_description_value AS description, algorithm.field_algorithm_times_tested_value AS timestested, algorithm.field_algorithm_tested_puzzles_value AS tested, u.uid AS uid, u.name AS username, u.picture AS userpicture, n.nid AS id, nr.body FROM content_type_algorithm_wars_algorithms algorithm LEFT JOIN node n ON algorithm.nid=n.nid LEFT JOIN node_revisions nr ON n.vid=nr.vid LEFT JOIN users u ON u.uid=n.uid WHERE n.nid=$nid"
+		$where = "WHERE rating >= $ratingA AND rating <= $ratingB";
+		$result = db_query("$query $where");
+		$algorithms = array();
+		while($res = db_fetch_array($result))
+			array_push($algorithms, $res);
+
+		return $algorithms;
 	}
 
 	//Gets all the algorithms by a particular rating
 	function get_algorithms_by_rating($rating){
-		//Implementation
+		$query = "SELECT n.title, n.created, algorithm.field_algorithm_rating_value AS rating, algorithm.field_algorithm_code_value AS code, algorithm.field_algorithm_votes_value AS numvotes, algorithm.field_algorithm_description_value AS description, algorithm.field_algorithm_times_tested_value AS timestested, algorithm.field_algorithm_tested_puzzles_value AS tested, u.uid AS uid, u.name AS username, u.picture AS userpicture, n.nid AS id, nr.body FROM content_type_algorithm_wars_algorithms algorithm LEFT JOIN node n ON algorithm.nid=n.nid LEFT JOIN node_revisions nr ON n.vid=nr.vid LEFT JOIN users u ON u.uid=n.uid WHERE n.nid=$nid"
+		$where = "WHERE rating = $rating";
+		$result = db_query("$query $where");
+		$algorithms = array();
+		while($res = db_fetch_array($result))
+			array_push($algorithms, $res);
+
+		return $algorithms;
 	}
 
 	//Gets the top numofAlgorithms algorithms based on the number of votes
 	function get_top_voted_algorithms($numofAlgorithms){
-		//Implementation
+		$query = "SELECT n.title, n.created, algorithm.field_algorithm_rating_value AS rating, algorithm.field_algorithm_code_value AS code, algorithm.field_algorithm_votes_value AS numvotes, algorithm.field_algorithm_description_value AS description, algorithm.field_algorithm_times_tested_value AS timestested, algorithm.field_algorithm_tested_puzzles_value AS tested, u.uid AS uid, u.name AS username, u.picture AS userpicture, n.nid AS id, nr.body FROM content_type_algorithm_wars_algorithms algorithm LEFT JOIN node n ON algorithm.nid=n.nid LEFT JOIN node_revisions nr ON n.vid=nr.vid LEFT JOIN users u ON u.uid=n.uid WHERE n.nid=$nid"
+		$order = "ORDER BY numvotes DESC";
+		$limit = "LIMIT $numofAlgorithms";
+		$full_query = "$query $order $limit";
+		$result = db_query($full_query);
+		$algorithms = array();
+		while($res = db_fetch_array($result))
+			array_push($algorithms, $res);
+
+		return $algorithms;
 	}
 
 	//Gets the rating of a particular algorithm
 	function get_algorithm_rating($id){
-		//Implementation
+		$algorithm = get_algorithm($id);
+		return $algorithm["rating"];
 	}
 
 	//Get Algorithm ranking
 	function get_algorithm_ranking($id){
-		//Implementation
+		$numrows = db_result(db_query("SELECT COUNT(*) FROM content_type_algorithm_wars_algorithms"));
+		$algorithms = get_top_voted_algorithms($numrows);
+		for($i = 0; $i < $numrows; $i++) {
+			if($algorithms[$i]["id"] == $id) return ($i + 1);
+		}
+		return null;
 	}
 
 	##################################
