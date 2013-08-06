725d455dc571b8cc5c3c8d28b7672eae9b4f47f4
Vineet Kosaraju <vineetsk1gmail.com>
2013-08-06 14:31:15 -0700

    initial commit of cron job, will need to check later

--- cron_job.php
+++ cron_job.php
@@ -1,5 +1,47 @@
-<? php
+<?php
 
-// Cron job handler to run evaluation of scripts at a particular time
+// php cron job
+	// Absolute require paths
+	// To run:
+	// crontab -e
+	// 0 22 * * * /usr/local/bin/php /absolute/path/to/cron_job.php
+	// This will run it once a day, at 10 PM
+	
+	function getScripts() {
+		$f = "vineet.eternadev.org/get/?type=awscript";
+		$data = file_get_contents($f);
+		return json_decode($data);
+	}
+
+	function getPuzzles() {
+		$f = "vineet.eternadev.org/get/?type=awpuzzle";
+		$data = file_get_contents($f);
+		return json_decode($data);
+	}
+
+	function sendData($scripts, $puzzles) {
+		$myscripts = $scripts['data']['algorithmlist'];
+		$mypuzzles = $puzzles['data']['puzzlelist'];
+
+		$url = "linktojustinserver.com";
+		$vars = 'scripts=' . $myscripts . '&puzzles=' . $mypuzzles;
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $vars);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+		return curl_exec($ch);
+	}
+
+	function main() {
+		$scripts = getScripts();
+		$puzzles = getPuzzles();
+		sendData($scripts, $puzzles);
+	}
+
+	main();
 
 ?>
