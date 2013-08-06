96461119f3216c1f87c6e6eb4ab47c8a2741a8c6
Vineet Kosaraju <vineetsk1gmail.com>
2013-08-06 12:36:07 -0700

    updating with get for algorithms

--- competition_controller.php
+++ competition_controller.php
@@ -4,4 +4,60 @@
 // Note that functions in this file will ultimately have to be extracted
 // out and added to eterna_get_controller.php and eterna_post_controller.php
 
+class EternaGETController extends EternaController {
+
+	function __construct() { 
+		parent::__construct();
+	}
+
+	function on_process($params) {
+		global $user;
+
+		$ret = array();
+		$type = $params['type'];
+		$data = array();
+
+		if($type == "awalgorithms") {
+
+			$user_model = $this->get_model("EternaUserModel");
+      		$algorithm_model = $this->get_model("EternaAlgorithmsModel");      
+      		$puzzle_model = $this->get_model("EternaPuzzleModel");
+
+      		$func = $params["func"];
+
+      		if(!$func) {
+      			$nid = $params["nid"];
+      			$data['ret'] = $algorithm_model->get_algorithm($nid);
+      		} else if($func == "between") {
+      			$ratingA = $params["above"];
+      			$ratingB = $params["below"];
+      			$data['ret'] = $algorithm_model->get_algorithms_between($ratingA, $ratingB);
+      		} else if($func == "byrating") {
+      			$rating = $params["rating"];
+      		} else if($func == "top") {
+      			$rating = $params["num"];
+      		} else if($func == "rating") {
+      			$rating = $params["id"];
+      		} else if($func == "all") {
+
+      		} else if($func == "ranking") {
+      			$rating = $params["id"];
+      		} else if($func == "queue") {
+
+      		} else if($func == "next") {
+
+      		}
+
+
+
+
+			$nid = $parmas["bla"];
+		} else if($type == "awpuzzles") {
+
+		}
+
+	}
+}
+
+
 ?>
