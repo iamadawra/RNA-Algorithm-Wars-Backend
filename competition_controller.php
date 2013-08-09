f8b17562931eb337b12d60431b9c9b0394728930
Vineet Kosaraju <vineetsk1gmail.com>
2013-08-09 14:32:01 -0700

    adding competition controller

--- competition_controller.php
+++ competition_controller.php
@@ -24,33 +24,10 @@ class EternaGETController extends EternaController {
 		$algorithm_model = $this->get_model("EternaAlgorithmsModel");      
 		$puzzle_model = $this->get_model("EternaPuzzleModel");
 
-		if($type == "awpuzzles") {
+		if($type == "awalgorithms") {
 			$func = $params["func"];
-			if($func == "reset") {
-				$puzzle_model->resetQueue($params["nummatches"], -1); // 100 matches?
-			} else if($func == "resetAll") {
-				$puzzle_model->reset_all_puzzles();
-			} else if($func == "update") {
-				$puzzle_model->update_puzzle_rating($params["aid"], $params["pid"], $params["ascore"], $params["pscore"]);
-			}
-		} else if($type == "awalgorithms") {
-			$func = $params["func"];
-			if($func == "reset") {
-				$algorithm_model->resetQueue($params["nummatches"]);
-			} else if($func == "add") {
+			if($func == "add") {
 				$algorithm_model->add_algorithm($params, $params["uid"], $user_model);
-			} else if($func == "delete") {	
-				$algorithm_model->delete_algorithm($params["aid"]);
-			} else if($func == "modify") {
-				$algorithm_model->modify_algorithm($params["aid"], $params["source"]);
-			} else if($func == "setrating") {
-				$algorithm_model->set_rating($params["rating"], $params["aid"]);
-			} else if($func == "setall") {
-				$algorithm_model->set_default_ratings_for_all($params["defaultrating"]);
-			} else if($func == "update") {
-				$algorithm_model->update_algorithm_rating($params["aid"], $params["pid"], $params["ascore"], $params["pscore"]);
-			} else if($func == "addQueue") {
-				$algorithm_model->add_algorithms_to_queue($params["id"]);
 			}
 		} else if($type == "awupdate") {
 			// justin's cron is sending us data
@@ -85,50 +62,24 @@ class EternaGETController extends EternaController {
 				if(!$func) {
 					$nid = $params["nid"];
 					$data['ret'] = $puzzle_model->get_puzzle($nid);
-				} else if($func == "next") {
-
-				} else if($func == "favored") {
-					$data['ret'] = $puzzle_model->get_favored_puzzles($params["num"]);
-				} else if($func == "rated") {
-					$data['ret'] = $puzzle_model->get_rated_puzzles($params["min"], $params["max"]);
-				} else if($func == "tested") {
-					$data['ret'] = $puzzle_model->get_least_tested_puzzles($params["num"], $params["constraints"]);
-				} else if($func == "rating") {
-					$data['ret'] = $puzzle_model->get_puzzle_rating($params["pid"]);
-				} else if($func == "type") {
-					if(method_exists($puzzle_model, "get_" . $params["level"] . "_puzzles"))
-						$data['ret'] = call_user_func( array($puzzle_model, "get_" . $params["level"] . "_puzzles"));
-					else
-						$data['ret'] = "Function not found in line 51 of competition_controller :(";				
+				} else if($func == "top") {
+					$data['ret'] = $puzzle_model->get_top_ranked_puzzles($params["num"]);
+				} else if($func == "queue") {
+					$data['ret'] = $puzzle_model->get_current_puzzle_queue();
 				}
 			} else if($type == "awalgorithms") {
-				// DONE.
 
 				$func = $params["func"];
 
 				if(!$func) {
 					$nid = $params["nid"];
 					$data['ret'] = $algorithm_model->get_algorithm($nid);
-				} else if($func == "between") {
-					$ratingA = $params["above"];
-					$ratingB = $params["below"];
-					$data['ret'] = $algorithm_model->get_algorithms_between($ratingA, $ratingB);
-				} else if($func == "byrating") {
-					$rating = $params["rating"];
-					$data['ret'] = $algorithm_model->get_algorithms_by_rating($rating);
 				} else if($func == "top") {
 					$num = $params["num"];
-					$data['ret'] = $algorithm_model->get_top_voted_algorithms($num);
-				} else if($func == "rating") {
+					$data['ret'] = $algorithm_model->get_top_ranked_algorithms($num);
+				} else if($func == "queue") {
 					$id = $params["id"];
-					$data['ret'] = $algorithm_model->get_algorithm_rating($id);
-				} else if($func == "all") {
-					$data['ret'] = $algorithm_model->get_all_algorithms();
-				} else if($func == "ranking") {
-					$id = $params["id"];
-					$data['ret'] = $algorithm_model->get_algorithm_ranking($id);
-				} else if($func == "next") {
-					$data['ret'] = $algorithm_model->get_next_algorithm_in_queue();
+					$data['ret'] = $algorithm_model->get_current_algorithm_queue($id);
 				}
 		}
 	}
